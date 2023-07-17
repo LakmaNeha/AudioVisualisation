e78c69afb2b1fd160401f4131a7a8796f096d690
Neha Lakma <50057358+LakmaNeha@users.noreply.github.com>
2023-07-17 18:33:41 +0000

    feat: Partial implementation of audio visualisation with highcharts error 12
    - Get channel data from decodedAudio
    - Extract waveform from channel data
    - Convert audio data into waveform points
    - Pass options to Highchart to visualise wave.

--- src/audioVisualisation/AudioVisualisation.tsx
+++ src/audioVisualisation/AudioVisualisation.tsx
@@ -1,8 +1,43 @@
-import React from "react";
-import Highcharts from "highcharts";
+import React, {useState,useEffect} from "react";
+import Highcharts, { getOptions } from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
 const AudioVisualisation = () => {
+
+  const [waveformPoints, setWaveformPoints] = useState<Array<any>>([]);
+  const [chartOptions, setChartOptions] = useState<any>({});
+
+  const getOptions = () => {
+    return {
+      chart: {
+        type: "line",
+      },
+      title: {
+        text: "Waveform",
+      },
+      xAxis: {
+        title: {
+          text: "Time",
+        },
+      },
+      yAxis: {
+        title: {
+          text: "Amplitude",
+        },
+      },
+      legend: {
+        enabled: false,
+      },git add
+      series: [
+        {
+          data: waveformPoints || [],
+          lineWidth: 4,
+          color: "green",
+        },
+      ],
+    };
+  };
+
   const decodeAudioFile = (file) => {
     return new Promise((resolve, reject) => {
       const fileReader = new FileReader();
@@ -29,6 +64,19 @@ const AudioVisualisation = () => {
       fileReader.readAsArrayBuffer(file);
     });
   };
+  const convertAudioToWaveformPoints = (audioData) => {
+    const waveformPoints: any = [];
+  
+    for (let i = 0; i < audioData.length; i++) {
+      const point = {
+        x: i,
+        y: audioData[i],
+      };
+      waveformPoints.push(point);
+    }
+  
+    return waveformPoints;
+  };
 
   const handleFileUpload = async (e) => {
     const file = e.target.files[0];
@@ -36,17 +84,27 @@ const AudioVisualisation = () => {
     decodeAudioFile(file)
       .then((decodedAudioData) => {
         console.log("Audio file decoded:", decodedAudioData);
+        const channelData = (decodedAudioData as any).getChannelData(0);
+        console.log('channelData:', channelData);
+        const waveformPoints = convertAudioToWaveformPoints(channelData);
+        console.log('waveformPoints:', waveformPoints);
+        setWaveformPoints(waveformPoints);
       })
       .catch(function (error) {
         console.error("Error decoding audio file:", error);
       });
   };
 
+  useEffect(()=>{
+    const options = getOptions();
+    setChartOptions(options);
+  },[waveformPoints])
+
   return (
     <div>
       <div>AudioVisualisation</div>
       <input type="file" onChange={handleFileUpload} />
-      <HighchartsReact highcharts={Highcharts} options={{}} />
+      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
     </div>
   );
 };
