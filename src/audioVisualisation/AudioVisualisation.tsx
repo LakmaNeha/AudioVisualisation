7847c337d9a26f85e7d309bad12ece0650aa7b45
Neha Lakma <50057358+LakmaNeha@users.noreply.github.com>
2023-07-17 17:20:22 +0000

    feat: Decodes the audio file using AudioContext

--- src/audioVisualisation/AudioVisualisation.tsx
+++ src/audioVisualisation/AudioVisualisation.tsx
@@ -1,9 +1,38 @@
 import React from "react";
 
 const AudioVisualisation = () => {
+
+  const decodeAudioFile = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.onload = function () {
+        const arrayBuffer: any = this.result;
+        const audioContext = new window.AudioContext();
+
+        audioContext.decodeAudioData(
+          arrayBuffer,
+          (decodedAudioBuffer) => {
+            console.log(decodedAudioBuffer, "#decodedAudioBuffer");
+            resolve(decodedAudioBuffer);
+          },
+          (error) => {
+            reject(error);
+          },
+        );
+      };
+
+      fileReader.onerror = function (error) {
+        reject(error);
+      };
+
+      fileReader.readAsArrayBuffer(file);
+    });
+  };
+
   const handleFileUpload = async (e) => {
     const file = e.target.files[0];
     console.log(file, "##file");
+    decodeAudioFile(file);
   };
 
   return (
